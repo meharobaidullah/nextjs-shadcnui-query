3e32e2af6b82c87555ce6546d3e3a6df686f3ebc
Obaidullah Ishfaq <obaidullahishfaq@gmail.com>
2024-02-04 00:20:09 +0500

    Setup shadcn-ui libarary and add table component.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter as FontSans } from "next/font/google";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { cn } from "@/lib/utils";
 
 // Create a client
 const queryClient = new QueryClient();
 
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+    <main
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
 
-      {/* By default, React Query Devtools are not included in production build */}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        {/* By default, React Query Devtools are not included in production build */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </main>
   );
 }
